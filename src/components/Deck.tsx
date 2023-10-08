b549c85eb721c95c068b6891fb119a5f862e7ec4
Arunaabh Pant <91041789+arunaabh95@users.noreply.github.com>
2023-10-08 01:10:58 -0400

    Parse LLM Output
    1 Use text model instead of chat. 
    2 Simplify flashcard prompt to answer generic queries 
    3 Parse each type of query accordingly (#3)

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { CardType, DeckCategory } from '../types/type';
+import { DeckCategory } from '../types/type';
 import { FlashcardProps} from './Flashcard';
 import Flashcard from './Flashcard';
 
